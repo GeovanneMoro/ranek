9d434279caace16d09927afd1fe21189d681f3c5
Geovanne Moro <geovannemoro@hotmail.com>
2020-10-09 16:19:14 -0300

    Updates

---
+++ precache-manifest.13b8d39828b2d504d52116d4e84b0393.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9937a8e1425bec14ff3d21f0fef2c1b2",
+    "url": "/ranek/index.html"
+  },
+  {
+    "revision": "a491833437844721cc58",
+    "url": "/ranek/static/css/main.db480485.chunk.css"
+  },
+  {
+    "revision": "2fab33741adea42d134b",
+    "url": "/ranek/static/js/2.ae4fbcd5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ranek/static/js/2.ae4fbcd5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a491833437844721cc58",
+    "url": "/ranek/static/js/main.2a08e3b8.chunk.js"
+  },
+  {
+    "revision": "87c20b8a884880e787e0",
+    "url": "/ranek/static/js/runtime-main.162079f5.js"
+  },
+  {
+    "revision": "20d1d7bb569e9158b47473eba9c4ca2c",
+    "url": "/ranek/static/media/contato.20d1d7bb.jpg"
+  }
+]);
